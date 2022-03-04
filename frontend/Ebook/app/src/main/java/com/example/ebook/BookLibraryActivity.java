728794eb5c8bf4ca2e256d06ca443f2bbf6b58cf
QuynhDuong <quynhduong@la1113.lib.iastate.edu>
2022-03-04 16:10:50 -0600

    activity page

--- frontend/Ebook/app/src/main/java/com/example/ebook/BookLibraryActivity.java
+++ frontend/Ebook/app/src/main/java/com/example/ebook/BookLibraryActivity.java
@@ -2,13 +2,24 @@ package com.example.ebook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class BookLibraryActivity extends AppCompatActivity {
-
+    ImageView backButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_library);
+        backButton = findViewById(R.id.backButtonBook);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(BookLibraryActivity.this, HomeActitvity.class);
+                startActivity(i);
+            }
+        });
     }
 }
